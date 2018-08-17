a498d3c60f1c3571c9716babc2433c416a2d7f22
Aubrey Cook <aubreyjcook@gmail.com>
2018-08-17 12:24:54 -0500

    loader files created, gitignore updated

---
+++ src/php/footerLoader.php
@@ -0,0 +1,4 @@
+<?php  /* This file is responisible for loading functions and methods that appear at the end of section of the document, it should appear in every page. 
+        * This file should primarily be used to load JavaScript files.
+        */
+?>
