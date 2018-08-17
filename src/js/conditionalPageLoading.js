f52777775447652f3d1d5cb7c4f264b49b2cbe94
Aubrey Cook <aubreyjcook@gmail.com>
2018-08-17 14:32:24 -0500

    js functions deleted, js variables created

--- src/js/conditionalPageLoading.js
+++ src/js/conditionalPageLoading.js
@@ -1,36 +1,10 @@
 /* variable declarations
  ------------------------------------------------------------- */
-let current_doc_name = window.location.pathname.split("/").pop();
+/* this gets the current working document name and extension as a variable, eg. "index.php" */
+let current_page = window.location.pathname.split("/").pop();
+/* this gets the page name without the extension: eg. 'index.php' = 'index' */
+let current_doc_name = current_page.split(".").slice(0, -1).join(".");
 
 
 /* function/method declarations
  ------------------------------------------------------------- */
-/* function loadCSSFile(fileName) {
-    //get the site domain and current path url
-    var siteURL = document.location.origin + document.location.pathname;
-    siteURL = siteURL.substring(0, siteURL.lastIndexOf("/"));
-
-    //ajax method that checks for the existence of a css file in the /css directory based on fileName string, loads it if it exists
-    $.ajax({
-        url: siteURL + '/css/' + fileName + '.css',
-        type: 'HEAD',
-        error: function() {
-            //file does not exist
-        },
-        success: function() {
-            //file exists
-            $('head').append('<link rel="stylesheet" href="css/' + fileName + '.css">');
-        }
-    });
-}
-
-function loadPageCSS() {
-    //get the current page name + extension
-    var currentDocument = location.href.split("/").slice(-1);
-    //remove the file extension
-    currentDocument = currentDocument.toString().replace(/\.[^/.]+$/, "");
-
-    if (currentDocument.toString() !== '') {
-        loadCSSFile(currentDocument);
-    }
-} */
